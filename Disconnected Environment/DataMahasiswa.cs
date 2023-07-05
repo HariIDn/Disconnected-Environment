92dac0eda24fcc547d41699a3f18ab641062d51d
HariIDn <aldifaadit75@gmail.com>
2023-07-05 19:28:11 +0700

    Add project files.

---
+++ Disconnected Environment/DataMahasiswa.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Disconnected_Environment
+{
+    public partial class DataMahasiswa : Form
+    {
+        private string stringConnection = "data source=Aldifa\\ADITHARI;" +
+        "database=PABD;User ID=sa;Password=123";
+        private SqlConnection koneksi;
+        private string nim, nama, alamat, jk, prodi;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private DateTime tgl;
+        BindingSource customersBindingSource = new BindingSource();
+        public DataMahasiswa()
+        {
+            InitializeComponent();
+        }
+
+        private void DataMahasiswa_Load(object sender, EventArgs e)
+        {
+            koneksi.Open();
+            SqlDataAdapter dataAdapter1 = new SqlDataAdapter(new SqlCommand("Select m.nim, m.nama_mahasiswa, " +
+                "m.alamat, m.jenis_kel, m.tgl_lahir, p.nama_prodi From dbo.Mahasiswa m" +
+                "join dbo.Prodi p on m.id_prodi = p.id_prodi", koneksi));
+            DataSet ds = new DataSet();
+            dataAdapter1.Fill(ds);
+
+            this.customersBindingSource.DataSource = ds.Tables[0];
+            this.txtNIM.DataBindings.Add(
+                new Binding("Text", this.customersBindingSource, "NIM", true));
+            this.txtNama.DataBindings.Add(
+                new Binding("Text", this.customersBindingSource, "nama_mahasiswa", true));
+            this.txtAlamat.DataBindings.Add(
+                new Binding("Text", this.customersBindingSource, "alamat", true));
+            this.cbxJenisKelamin.DataBindings.Add(
+                new Binding("Text", this.customersBindingSource, "jenis_kel", true));
+            this.dtTanggalLahir.DataBindings.Add(
+                new Binding("Text", this.customersBindingSource, "tgl_lahir", true));
+            this.cbxProdi.DataBindings.Add(
+                new Binding("Text", this.customersBindingSource, "nama_prodi", true));
+            koneksi.Close();
+        }
+        private void clearBinding()
+        {
+            this.txtNIM.DataBindings.Clear();
+            this.txtNama.DataBindings.Clear();
+            this.txtAlamat.DataBindings.Clear();
+            this.cbxJenisKelamin.DataBindings.Clear();
+        }
+    }
+}
